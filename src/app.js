3afb7d7c1c7f05fe31a1bef47ea5e3951219d458
Sai Tharun Dusa <sd2672@nau.edu>
2025-01-07 23:51:21 -0700

    cors error resolved

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {connectDB} = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
@@ -10,7 +11,10 @@ const userRouter = require("./routes/user");
 
 const app = express();
 
-
+app.use(cors({
+    origin: 'https://localhost:5173',
+    credentials: true,
+}))
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -48,7 +48,7 @@ authRouter.post("/login", async(req, res)=>{
             res.cookie("token", token, {
                 expires: new Date(Date.now() + 8 * 3600000),
             });
-            res.json({message : "Login Successful!!"});
+            res.json({message : "Login Successful!!", data:user});
         }
     }
     catch(err)
